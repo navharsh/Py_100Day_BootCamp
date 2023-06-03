67b8827afbe1c448506adde4ba23e18ca2cc9d17
Navharsh <navharshkumar12@gmail.com>
2023-06-03 06:30:05 +0530

    Added D6

---
+++ Day6/hurdles_ch4.py
@@ -0,0 +1,17 @@
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not is_facing_north():
+    turn_right()
+while not at_goal():
+
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
